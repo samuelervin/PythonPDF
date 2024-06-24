a0685d967d3fcf19255c9b5d02f21f7f51a7b7ff
Sam Ervin <servin2@theark.cloud>
2024-06-24 13:14:53 -0400

    Added note lines

--- main.py
+++ main.py
@@ -9,23 +9,35 @@ pdf = FPDF(orientation='portrait', unit='mm', format='A4')
 pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
+    #adding header to the page
     pdf.add_page()  # Add a page
     pdf.set_font(family="Times",style="B", size=24)  # Set font for the
     pdf.set_text_color(100, 100, 100)  # Set text color
     pdf.cell(w=0,h=12, txt=row['Topic'],align="L",ln=1)  # Add a cell for header
-    pdf.line(10, 21, 200, 21)  # Draw a line
 
-
+    #adding note lines
+    for y in range(20,298,10):
+        pdf.line(10, y, 200, y)  # Draw lines for pages
+   
+   #Adding a footer to the page     
     pdf.ln(265) #add space for the topics
     pdf.set_font(family="Times",style="I", size=8)  # Set font for the
     pdf.set_text_color(180, 180, 180)  # Set text color  
     pdf.cell(w=0,h=10, txt=row['Topic'],align="R")  # Add a cell for footer
 
     for i in range(row["Pages"] - 1): #add pages from the topics
+       # add new page per number of pages per topic
         pdf.add_page()
-        pdf.ln(277) #add space for the topics
+        
+        #adding note lines
+        for y in range(20,298,10):
+            pdf.line(10, y, 200, y)  # Draw lines for pages
+        
+        #add footer to the topic pages
+        pdf.ln(277) #space for the topics
         pdf.set_font(family="Times",style="I", size=8)  # Set font for the
         pdf.set_text_color(180, 180, 180)  # Set text color  
         pdf.cell(w=0,h=10, txt=row['Topic'],align="R")  # Add a cell for footer
 
+#output pdf to a file
 pdf.output("topics.pdf")  # Output the pdf
