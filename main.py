e05b4684c8331f4171983f7d7f29733f818b4f5c
Sam Ervin <servin2@theark.cloud>
2024-06-24 12:41:29 -0400

    Added ability to add multiple pages from the topics

--- main.py
+++ main.py
@@ -14,5 +14,7 @@ for index, row in df.iterrows():
     pdf.cell(w=0,h=12, txt=row['Topic'],align="L",ln=1)  # Add a cell
     pdf.line(10, 21, 200, 21)  # Draw a line
 
+    for i in range(row["Pages"] - 1): #add pages from the topics
+        pdf.add_page()
 
 pdf.output("topics.pdf")  # Output the pdf
